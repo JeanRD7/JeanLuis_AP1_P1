5c7f2d911f27eb704c1975bffbdb247c7bb9f45d
JeanRD7 <jean64098@gmail.com>
2023-09-25 21:07:57 -0400

    Inyectando Context en el Program

---
+++ DAL/Context.cs
@@ -0,0 +1,10 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace JeanLuis_AP1_P1.DAL
+{
+    public class Context: DbContext
+    {
+        public Context(DbContextOptions<Context>opcion): base(opcion) { }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+using JeanLuis_AP1_P1.DAL;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+
+builder.Services.AddDbContext<Context>(op => op.UseSqlite(ConStr));
 
 var app = builder.Build();
 
