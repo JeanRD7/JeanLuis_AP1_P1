ba36b36377a092f4cdc9e9add8e1a4083977be70
JeanRD7 <jean64098@gmail.com>
2023-10-02 18:50:20 -0400

    Creacion del Modelo

---
+++ Models/Aportes.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+
+namespace JeanLuis_AP1_P1.Models
+{
+    public class Aportes
+    {
+        [Key]
+
+        public int AporteId { get; set; }
+
+        [Required (ErrorMessage ="Debe Registrar la fecha de Aporte")]
+        public DateTime Fecha { get; set; } = new DateTime();
+        [Required (ErrorMessage ="Debe llenar el campo.")]
+        public string? Persona { get; set; }
+        [Required (ErrorMessage ="Debe llenar el campo.")]
+        public string? Observacion { get; set; }
+        [Required(ErrorMessage = "Debe Ingresar un Monto.")]
+        public decimal? Monto { get; set; }
+    }
+}
